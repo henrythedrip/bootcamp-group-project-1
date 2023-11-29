32bac7768b774a436739000bf13d2a7392e1d848
henrythedrip <146283648+henrythedrip@users.noreply.github.com>
2023-11-28 19:56:55 -0700

    created local storage functionality for the forum.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,6 @@
 // Group Project 1 api:
 // AIzaSyDzc69XmD4DeT9-Te5GUZ-_6BwQgIyvkNw
 // Get the modal
-var inputEmail = document.getElementById("email");
-var inputName = document.getElementById("name");
-var inputBio = document.getElementById("bio");
 var modal = document.getElementById("modalForm");
 // Get the button that opens the modal
 var btn = document.getElementById("addMeButton");
@@ -11,16 +8,33 @@ var btn = document.getElementById("addMeButton");
 var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.addEventListener("click", function (event) {
-  event.preventDefault();
-  modal.style.display = "block";
+    event.preventDefault();
+    modal.style.display = "block";
 });
 // When the user clicks on <span> (x), close the modal
 span.onclick = function (event) {
-  modal.style.display = "none";
+    modal.style.display = "none";
 };
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == Submit) {
-    modal.style.display = "none";
-  }
+    if (event.target == Submit) {
+        modal.style.display = "none";
+    }
 };
+
+var email = document.getElementById("email");
+var namer = document.getElementById("name");
+var bio = document.getElementById("bio");
+
+function store() {
+localStorage.clear;
+var emailInput = email.value;
+localStorage.setItem("email", emailInput);
+var namerInput = namer.value;
+localStorage.setItem("name", namerInput);
+var bioInput = bio.value;
+localStorage.setItem("bio", bioInput);
+console.log(emailInput);
+console.log(namerInput);
+console.log(bioInput);
+}
